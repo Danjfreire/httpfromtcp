ec1e1c749f50704493f9012372425dabd1d2f7b7
Danjfreire <danjfreire@gmail.com>
2025-12-14 10:05:00 -0300

    feat: returning response body

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,20 +1,39 @@
 package server
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"sync/atomic"
 
+	"github.com/Danjfreire/httpfromtcp/internal/request"
 	"github.com/Danjfreire/httpfromtcp/internal/response"
 )
+
+type HandlerError struct {
+	Code    response.StatusCode
+	Message string
+}
+
+type Handler func(w io.Writer, req *request.Request) *HandlerError
+
+func (he *HandlerError) Write(w io.Writer) {
+	response.WriteStatusLine(w, he.Code)
+	body := []byte(he.Message)
+	headers := response.GetDefaultHeaders(len(body))
+	response.WriteHeaders(w, headers)
+	w.Write(body)
+}
 
 type Server struct {
 	listener net.Listener
 	closed   atomic.Bool
+	handler  Handler
 }
 
-func Serve(port int) (*Server, error) {
+func Serve(port int, handler Handler) (*Server, error) {
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%v", port))
 	if err != nil {
 		return nil, err
@@ -22,6 +41,7 @@ func Serve(port int) (*Server, error) {
 
 	s := &Server{
 		listener: listener,
+		handler:  handler,
 	}
 
 	go s.listen()
@@ -53,15 +73,30 @@ func (s *Server) listen() {
 
 func (s *Server) handle(conn net.Conn) {
 	defer conn.Close()
-	headers := response.GetDefaultHeaders(0)
 
-	err := response.WriteStatusLine(conn, response.StatusOk)
+	req, err := request.RequestFromReader(conn)
 	if err != nil {
-		fmt.Println("Failed to write status line")
+		log.Fatal("Failed to extract request from connection")
+	}
+
+	var bodyBuf bytes.Buffer
+	handlerErr := s.handler(&bodyBuf, req)
+	if handlerErr != nil {
+		handlerErr.Write(conn)
+		return
+	}
+
+	headers := response.GetDefaultHeaders(bodyBuf.Len())
+
+	err = response.WriteStatusLine(conn, response.StatusOk)
+	if err != nil {
+		fmt.Println("failed to write status line")
 	}
 
 	err = response.WriteHeaders(conn, headers)
 	if err != nil {
-		fmt.Println("Failed to write response headers")
+		fmt.Printf("failed to write response headers: %v\n", err)
 	}
+
+	conn.Write(bodyBuf.Bytes())
 }
